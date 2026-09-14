9a812199ab25c028b75f4f6ae050d0bf2a7deede
ubgroupub-blip <ubgroupub@gmail.com>
2026-09-14 19:54:54 +0800

    Update RegistrationModal.tsx

--- src/components/RegistrationModal.tsx
+++ src/components/RegistrationModal.tsx
@@ -174,7 +174,7 @@ export const RegistrationModal: React.FC<RegistrationModalProps> = ({
 const googleSheetWebhookUrl = import.meta.env.VITE_GOOGLE_SHEET_WEBHOOK_URL;
 
 if (googleSheetWebhookUrl) {
-  fetch(googleSheetWebhookUrl, {
+await fetch(googleSheetWebhookUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'text/plain;charset=utf-8',
